e1bd7fbe481ad81d8d5b52c8d9adb9fe21ffad15
KenyaSaitoh <kenya.saitoh@outlook.jp>
2017-09-23 18:03:21 +0900

    commit

---
+++ 04_spring_mvc_person/src/main/java/person/PersonService.java
@@ -0,0 +1,97 @@
+package org.study.spring.mvc.person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+
+import org.springframework.stereotype.Component;
+
+@Component
+// @Dependent
+public class PersonService {
+    private List<Person> personList = new CopyOnWriteArrayList<Person>();
+
+    @PostConstruct
+    public void init() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classLoader.getResourceAsStream("persons.csv");
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                Person person = createPersonFromLine(line);
+                personList.add(person);
+            }
+        } catch(IOException ioe) {
+            throw new RuntimeException(ioe);
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch(IOException ioe) {
+                throw new RuntimeException(ioe);
+            }
+        }
+    }
+
+    public Person getPerson(Integer personId) {
+        for (Person person : personList) {
+            if (person.getPersonId().equals(personId)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public void addPerson(Person person) {
+        person.setPersonId(getNextPersonId());
+        personList.add(person);
+    }
+
+    public void removePerson(Integer personId) {
+        for (Person p : personList) {
+            if (p.getPersonId().equals(personId)) {
+                personList.remove(p);
+                return;
+            }
+        }
+    }
+
+    public void updatePerson(Person person) {
+        for (int i = 0; i < personList.size(); i++) {
+            if (personList.get(i).getPersonId().equals(person.getPersonId())) {
+                personList.remove(i);
+                personList.add(i, person);
+                return;
+            }
+        }
+        personList.add(person);
+    }
+
+    private Person createPersonFromLine(String line) {
+        StringTokenizer st = new StringTokenizer(line, ",");
+        Integer personId = Integer.parseInt(st.nextToken());
+        String personName = st.nextToken();
+        Integer age = Integer.parseInt(st.nextToken());
+        String gender = st.nextToken();
+        Person person = new Person(personId, personName, age, gender);
+        return person;
+    }
+
+    private Integer getNextPersonId() {
+        return personList.get(personList.size() - 1).getPersonId() + 1;
+    }
+}
