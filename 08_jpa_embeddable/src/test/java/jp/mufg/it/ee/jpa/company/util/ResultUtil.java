9fd29af6180f3221b1fde33fa2795eda4cc7351d
KenyaSaitoh <kenya.saitoh@outlook.jp>
2017-10-02 17:21:07 +0900

    commit

---
+++ 08_jpa_embeddable/src/test/java/jp/mufg/it/ee/jpa/company/util/ResultUtil.java
@@ -0,0 +1,63 @@
+package jp.mufg.it.ee.jpa.company.util;
+
+import java.util.Iterator;
+import java.util.List;
+
+import jp.mufg.it.ee.jpa.company.entity.Department;
+import jp.mufg.it.ee.jpa.company.entity.Employee;
+
+public class ResultUtil {
+
+    public static void showEmployee(Employee employee) {
+        if (employee != null) {
+            System.out.print(employee.getEmployeeId() + " / ");
+            System.out.print(employee.getEmployeeName() + " / ");
+            if (employee.getDepartment() != null)
+                System.out.print(employee.getDepartment().getDepartmentName()
+                        + " / ");
+            else
+                System.out.print("null" + " / ");
+            System.out.print(employee.getJobType().toString() + " / ");
+            System.out.println(employee.getSalary());
+        }
+    }
+
+    public static void showEmployeeList(List<Employee> list) {
+        Iterator<Employee> i = list.iterator();
+        while (i.hasNext()) {
+            Employee employee = i.next();
+            showEmployee(employee);
+        }
+    }
+
+    public static void showDepartment(Department department) {
+        if (department != null) {
+            System.out.print(department.getDepartmentId() + " / ");
+            System.out.print(department.getDepartmentName() + " / ");
+            System.out.print(department.getLocation());
+        }
+    }
+
+    public static void showDepartmentList(List<Department> list) {
+        Iterator<Department> i = list.iterator();
+        while (i.hasNext()) {
+            Department department = i.next();
+            showDepartment(department);
+        }
+    }
+
+    public static void showObjectArray(Object[] objs) {
+        for (Integer i = 0; i < objs.length; i++) {
+            System.out.print(objs[i] + " / ");
+        }
+        System.out.println();
+    }
+
+    public static void showObjectArrayList(List<Object[]> resultList) {
+        Iterator<Object[]> i = resultList.iterator();
+        while (i.hasNext()) {
+            Object[] objs = i.next();
+            showObjectArray(objs);
+        }
+    }
+}
---
+++ 08_jpa_lock/src/test/java/jp/mufg/it/ee/jpa/company/test/JpaOptimisticLockModeTypeTest.java
@@ -0,0 +1,39 @@
+package jp.mufg.it.ee.jpa.company.test;
+
+import javax.persistence.LockModeType;
+import javax.persistence.OptimisticLockException;
+import org.junit.Test;
+
+import jp.mufg.it.ee.jpa.company.entity.Employee;
+import jp.mufg.it.ee.jpa.company.test.base.JpaTestBase;
+
+// 楽観的ロックのテスト
+public class JpaOptimisticLockModeTypeTest extends JpaTestBase {
+
+    @Test
+    public void test1() {
+        System.out.println("[ test1 ] Start");
+
+        // 以下のいずれは、どれも同じ動きをする模様。
+        // Employee employee = em.find(Employee.class, 10016);
+        Employee employee = em.find(Employee.class, 10016,
+                LockModeType.OPTIMISTIC);
+        // Employee employee = em.find(Employee.class, 10016,
+        //        LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+
+        employee.setSalary(employee.getSalary() + 10000);
+        try {
+            Thread.sleep(20000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            em.flush();
+        } catch(OptimisticLockException ole) {
+            throw ole;
+            // リカバリ
+        }
+        commit();
+        System.out.println("[ test1 ] End\n");
+    }
+}
