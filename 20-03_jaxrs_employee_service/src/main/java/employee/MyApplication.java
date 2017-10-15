827c2c58836801dca80febf77759c982989e69de
KenyaSaitoh <kenya.saitoh@outlook.jp>
2017-10-15 15:08:45 +0900

    commit

--- 20-03_jaxrs_employee_service/src/main/java/employee/MyApplication.java
+++
@@ -1,8 +0,0 @@
-package org.study.ee.rs.server.employee;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/*")
-public class MyApplication extends Application {
-}
