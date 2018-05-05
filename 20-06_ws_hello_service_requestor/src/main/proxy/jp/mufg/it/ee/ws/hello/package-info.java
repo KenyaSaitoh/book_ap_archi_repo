ade5311af606c6dc15f337210ae74744a44eadec
KenyaSaitoh <kenya.saitoh@outlook.jp>
2018-05-05 15:33:39 +0900

    commit

--- 20-06_ws_hello_service_requestor/src/main/proxy/jp/mufg/it/ee/ws/hello/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://hello.ws.ee.it.mufg.jp/")
-package jp.mufg.it.ee.ws.hello;
--- 20-06_ws_mtom_service_requestor/src/main/proxy/jp/mufg/it/ee/ws/mtom/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://mtom.ws.ee.it.mufg.jp/")
-package jp.mufg.it.ee.ws.mtom;
