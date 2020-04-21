8045db6382ac8add8abe7be83ed1570dc5cf1e9f
KenyaSaitoh <kenya.saitoh@outlook.jp>
2020-04-21 20:09:52 +0900

    cc

--- 04-05_jsf_person_rdb/sql/derby/3_PERSON_DML.sql
+++
@@ -1,7 +0,0 @@
--- 一括削除
-DELETE FROM PERSON;
-
--- 人員テーブル
-INSERT INTO PERSON VALUES(1, 'Alice', 35, 'female');
-INSERT INTO PERSON VALUES(2, 'Bob', 20, 'male');
-INSERT INTO PERSON VALUES(3, 'Carol', 30, 'female');
